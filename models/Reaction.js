8a4c65f770b5b7c6d508f247b38137071bcaa352
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-06-06 17:19:13 -0500

    finished model for Reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,10 +1,33 @@
+const { Schema, Types } = require('mongoose');
 
-const mongoose = require('mongoose');
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
 
-const thoughtSchema = new mongoose.Schema({
-  name: String
-});
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
 
-const Thought = mongoose.model('Thought', thoughtSchema);
-module.exports = Thought;
+    username: {
+      type: String,
+      required: true,
+    },
 
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
