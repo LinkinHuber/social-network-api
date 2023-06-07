1658e28bef8581f9755a3ae58360037f75e00592
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-06-06 23:44:52 -0500

    added the rest of the routes to the userRoutes.js file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -32,6 +32,62 @@ router.get('/', async (req, res) => {
       res.status(400).json({err} )
     }
   });
-
   
+  router.put('/:id', async (req, res) => {
+    try{
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+            )
+        res.status(200).json({updateUser})
+    } catch(err){
+        console.log(err)
+        res.status(400).json({err})
+    }
+  })
+
+  router.delete('/:id', async (req, res) => {
+    try{
+        const deleteUser = await User.findByIdAndDelete(
+            req.params.id,
+            )
+        res.status(200).json({deleteUser})
+    } catch(err){
+        console.log(err)
+        res.status(400).json({err})
+    }
+  })
+
+  router.post('/:userId/friends/:friendId', async (req, res) => {
+    try {
+      const addFriend = await User.findOneAndUpdate(
+        {_id: req.params.userId},
+        {$addToSet: {friends: req.params.friendId}},
+        {new: true}
+        )
+      res.status(200).json({addFriend})
+
+    } catch(err){
+      console.log(err)
+      res.status(400).json({err} )
+    }
+  });
+
+  router.delete('/:userId/friends/:friendId', async (req, res) => {
+    try {
+      const deleteFriend = await User.findOneAndUpdate(
+        {_id: req.params.userId},
+        {$pull: {friends: req.params.friendId}},
+        {new: true}
+        )
+      res.status(200).json({deleteFriend})
+
+    } catch(err){
+      console.log(err)
+      res.status(400).json({err} )
+    }
+  });
+
+
 module.exports = router;
